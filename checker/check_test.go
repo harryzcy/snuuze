402d2661886018766b0d577edfe05313d7b32b1e
Chongyi Zheng <harry@harryzheng.com>
2023-03-09 22:54:36 -0500

    Use go command line to check package updates (#13)
    * Use go command line to check package updates
    
    * Remove checks for go modules

--- checker/check_test.go
+++ checker/check_test.go
@@ -4,47 +4,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"testing"
-
-	"github.com/harryzcy/latte/matcher"
-	"github.com/harryzcy/latte/types"
-	"github.com/stretchr/testify/assert"
 )
-
-func TestIsUpgradable(t *testing.T) {
-	tests := []struct {
-		dep  types.Dependency
-		want UpgradeInfo
-		err  error
-	}{
-		{
-			dep: types.Dependency{
-				Name:           "github.com/gofiber/fiber/v2",
-				Version:        "v2.40.0", // use an old version
-				PackageManager: matcher.GoMod,
-			},
-			want: UpgradeInfo{
-				Dependency: types.Dependency{
-					Name:           "github.com/gofiber/fiber/v2",
-					Version:        "v2.40.0",
-					PackageManager: matcher.GoMod,
-				},
-				Upgradable: true,
-			},
-		},
-	}
-
-	for i, test := range tests {
-		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			info, err := isUpgradable(test.dep)
-			assert.Equal(t, test.err, err)
-			assert.Equal(t, test.want.Upgradable, info.Upgradable)
-			assert.Equal(t, test.want.Dependency, info.Dependency)
-		})
-	}
-}
 
 func TestMain(m *testing.M) {
 	envFile := filepath.Join("..", ".env")
