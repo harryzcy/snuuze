8e6de7266ce3a3fe51e421848c36406573541991
harryzcy <harry@harryzheng.com>
2023-03-10 02:41:13 -0500

    Define configuration in struct

--- config/config.go
+++ config/config.go
@@ -6,6 +6,23 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Config struct {
+	Version string
+	Presets []string
+	Rules   []Rule
+}
+
+type Rule struct {
+	PackageManager string
+	PackageName    string
+	PackageType    string // direct, indirect, production, development, all
+	Labels         []string
+}
+
+var (
+	config Config
+)
+
 func Load() error {
 	viper.SetConfigName("snuuze")
 	viper.SetConfigType("yaml")
@@ -15,5 +32,18 @@ func Load() error {
 	if err != nil {
 		return fmt.Errorf("fatal error config file: %w", err)
 	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return fmt.Errorf("unable to decode into struct, %w", err)
+	}
 	return nil
+}
+
+func GetConfig() Config {
+	return config
+}
+
+func GetRules() []Rule {
+	return config.Rules
 }
