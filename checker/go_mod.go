aec420a875e4fe532fb5b4875a605959a2b4c874
harryzcy <harry@harryzheng.com>
2023-03-11 15:44:21 -0500

    Create util function for run commands

--- checker/go_mod.go
+++ checker/go_mod.go
@@ -1,14 +1,12 @@
 package checker
 
 import (
-	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"os"
-	"os/exec"
 	"time"
 
+	"github.com/harryzcy/snuuze/cmdutil"
 	"github.com/harryzcy/snuuze/types"
 )
 
@@ -33,45 +31,26 @@ type GoListOutput struct {
 }
 
 func isUpgradable_GoMod(dep types.Dependency) (types.UpgradeInfo, error) {
-	// run `go list -u -m -json <module>` to get the latest version]
-	cmd := exec.Command("go", "list", "-u", "-m", "-json", dep.Name)
-	cmd.Env = []string{
-		"GOPATH=" + os.Getenv("GOPATH"),
+	// run `go list -u -m -json <dep>` to get the latest version
+	envs := map[string]string{
+		"GOPATH": os.Getenv("GOPATH"),
 	}
 	if GOPROXY != "" {
-		cmd.Env = append(cmd.Env, "GOPROXY="+GOPROXY)
+		envs["GOPROXY"] = GOPROXY
 	}
-
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-
-	// set timeout
-	done := make(chan error, 1)
-	go func() {
-		done <- cmd.Run()
-	}()
-	select {
-	case <-time.After(DEFAULT_TIMEOUT):
-		if err := cmd.Process.Kill(); err != nil {
-			return types.UpgradeInfo{}, err
-		}
-		return types.UpgradeInfo{}, ErrRequestFailed
-	case err := <-done:
-		if err != nil {
-			fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-			return types.UpgradeInfo{}, err
-		}
+	output, err := cmdutil.RunCommand(cmdutil.CommandInputs{
+		Command: []string{"go", "list", "-u", "-m", "-json", dep.Name},
+		Env:     envs,
+	})
+	if err != nil {
+		return types.UpgradeInfo{}, err
 	}
 
 	// parse the output
 	// e.g. github.com/shurcooL/githubv4 v0.0.0-20221229060216-a8d4a561cc93 [v0.0.0-20230305132112-efb623903184]
 	// e.g. github.com/shurcooL/githubv4 v0.0.0-20221229060216-a8d4a561cc93
-	output := out.String()
-
 	info := GoListOutput{}
-	err := json.Unmarshal([]byte(output), &info)
+	err = json.Unmarshal(output.Stdout.Bytes(), &info)
 	if err != nil {
 		return types.UpgradeInfo{}, err
 	}
---
+++ cmdutil/cmdutil.go
@@ -0,0 +1,68 @@
+package cmdutil
+
+import (
+	"bytes"
+	"errors"
+	"os/exec"
+	"time"
+)
+
+const (
+	DEFAULT_TIMEOUT = 10 * time.Second
+)
+
+var (
+	ErrorTimeout = errors.New("timeout")
+)
+
+type CommandInputs struct {
+	Command []string
+	Env     map[string]string
+	Timeout time.Duration
+}
+
+func (input *CommandInputs) GetTimeout() time.Duration {
+	if input.Timeout == 0 {
+		return DEFAULT_TIMEOUT
+	}
+	return input.Timeout
+}
+
+type CommandOutput struct {
+	Stdout bytes.Buffer
+	Stderr bytes.Buffer
+}
+
+func RunCommand(inputs CommandInputs) (*CommandOutput, error) {
+	cmd := exec.Command(inputs.Command[0], inputs.Command[1:]...)
+	cmd.Env = []string{}
+	for key, value := range inputs.Env {
+		cmd.Env = append(cmd.Env, key+"="+value)
+	}
+
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	// set timeout
+	done := make(chan error, 1)
+	go func() {
+		done <- cmd.Run()
+	}()
+	select {
+	case <-time.After(inputs.GetTimeout()):
+		if err := cmd.Process.Kill(); err != nil {
+			return nil, err
+		}
+		return nil, ErrorTimeout
+	case err := <-done:
+		if err != nil {
+			return nil, err
+		}
+		return &CommandOutput{
+			Stdout: out,
+			Stderr: stderr,
+		}, nil
+	}
+}
