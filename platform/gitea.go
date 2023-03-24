d722591c20f8a60fc4fe3b62f6e57b3414b96a76
harryzcy <harry@harryzheng.com>
2023-03-24 04:55:50 -0400

    Support Gitea client

---
+++ platform/gitea.go
@@ -0,0 +1,130 @@
+package platform
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	GITEA_SERVER = strings.TrimSuffix(os.Getenv("GITEA_SERVER"), "/") // remove trailing slash
+	GITEA_TOKEN  = os.Getenv("GITEA_TOKEN")
+)
+
+const (
+	defaultTimeout = 10 * time.Second
+)
+
+type GiteaClient struct {
+	client http.Client
+}
+
+func NewGiteaClient(url, token string) Client {
+	return &GiteaClient{
+		client: http.Client{
+			Timeout: defaultTimeout,
+		},
+	}
+}
+
+func (c *GiteaClient) sendRequest(method, path string, body []byte) ([]byte, error) {
+	reader := bytes.NewReader(body)
+	req, err := http.NewRequest(method, path, reader)
+	if err != nil {
+		return nil, err
+	}
+
+	if method == "POST" {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	req.Header.Set("Authorization", "bearer "+GITEA_TOKEN)
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+type giteaTagList []struct {
+	Commit struct {
+		Created string `json:"created"`
+		Sha     string `json:"sha"`
+		URL     string `json:"url"`
+	} `json:"commit"`
+	ID         string `json:"id"`
+	Message    string `json:"message"`
+	Name       string `json:"name"`
+	TarballURL string `json:"tarball_url"`
+	ZipballURL string `json:"zipball_url"`
+}
+
+func (c *GiteaClient) ListTags(params *ListTagsInput) ([]string, error) {
+	// TODO: support pagination
+	url := GITEA_SERVER + "/api/v1/repos/" + params.Owner + "/" + params.Repo + "/tags"
+	data, err := c.sendRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var tags giteaTagList
+	err = json.Unmarshal(data, &tags)
+	if err != nil {
+		return nil, err
+	}
+
+	tagNames := make([]string, 0)
+	for _, tag := range tags {
+		tagNames = append(tagNames, tag.Name)
+	}
+
+	return tagNames, nil
+}
+
+type giteaCreatePullRequestOptions struct {
+	Assignee  string   `json:"assignee"`
+	Assignees []string `json:"assignees"`
+	Base      string   `json:"base"`
+	Body      string   `json:"body"`
+	DueDate   string   `json:"due_date"`
+	Head      string   `json:"head"`
+	Labels    []int64  `json:"labels"`
+	Milestone int64    `json:"milestone"`
+	Title     string   `json:"title"`
+}
+
+func (c *GiteaClient) CreatePullRequest(input *CreatePullRequestInput) error {
+	options := &giteaCreatePullRequestOptions{
+		Base:  input.Base,
+		Head:  input.Head,
+		Title: input.Title,
+		Body:  input.Body,
+	}
+
+	url := GITEA_SERVER + "/api/v1/repos/" + input.Owner + "/" + input.Repo + "/pulls"
+
+	body, err := json.Marshal(options)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.sendRequest(http.MethodPost, url, body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
