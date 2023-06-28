601fc55836b588eef66a1f7599bcf1a14dafb614
harryzcy <harry@harryzheng.com>
2023-06-27 20:55:57 -0500

    Exclude prerelease updates (#37)

--- checker/vcs_test.go
+++ checker/vcs_test.go
@@ -9,36 +9,68 @@ import (
 
 func TestGetLatestTag(t *testing.T) {
 	tests := []struct {
-		sortedTags []string
+		tags       []string
 		currentTag string
 		want       string
+		wantErr    bool
 	}{
 		{
-			sortedTags: []string{"v1.2.5", "v1.2.4", "v1.2.3"},
+			tags:       []string{"v1.2.5", "v1.2.4", "v1.2.3"},
 			currentTag: "v1.2.3",
 			want:       "v1.2.5",
 		},
 		{
-			sortedTags: []string{"v1.2.5", "v1.2.4", "v1.2.3"},
-			currentTag: "v1.2",
-			want:       "v1.2",
-		},
-		{
-			sortedTags: []string{"v1.2.5", "v1.2.4", "v1.2.3"},
+			tags:       []string{"v1.2.5", "v1.2.4", "v1.2.3"},
 			currentTag: "v1",
 			want:       "v1",
 		},
 		{
-			sortedTags: []string{"v2.2.5", "v1.2.4", "v1.2.3"},
+			tags:       []string{"v2.2.5", "v1.2.4", "v1.2.3"},
 			currentTag: "v1",
 			want:       "v2",
+		},
+		{
+			tags:       []string{"v1.2.4", "v2.2.5", "v1.2.3"},
+			currentTag: "v1",
+			want:       "v2",
+		},
+		{
+			tags:       []string{"v1.2.4", "v2.2.5-beta", "v1.2.3"},
+			currentTag: "v1",
+			want:       "v1",
+		},
+		{
+			tags:       []string{"v1.2.4", "v2.2.5-beta", "v1.2.3"},
+			currentTag: "v2",
+			want:       "v2",
+		},
+		{
+			currentTag: "invalid",
+			want:       "",
+			wantErr:    true,
+		},
+		{
+			tags:       []string{"invalid"},
+			currentTag: "v1",
+			want:       "",
+			wantErr:    true,
+		},
+		{
+			tags:       []string{"1.2.5"},
+			currentTag: "v1",
+			want:       "1",
 		},
 	}
 
 	for i, test := range tests {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			got := getLatestTag(test.sortedTags, test.currentTag)
+			got, err := getLatestTag(test.tags, test.currentTag)
 			assert.Equal(t, test.want, got)
+			if test.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
 		})
 	}
 }
