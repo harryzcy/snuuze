4e5c07560f0a663178ec33f7a02275d636a3899e
harryzcy <harry@harryzheng.com>
2023-06-27 20:51:46 -0500

    Exclude prerelease updates

--- checker/vcs.go
+++ checker/vcs.go
@@ -1,7 +1,11 @@
 package checker
 
 import (
+	"fmt"
+	"sort"
 	"strings"
+
+	"github.com/hashicorp/go-version"
 )
 
 type VSCSource interface {
@@ -9,12 +13,48 @@ type VSCSource interface {
 
 type GitHubSource struct{}
 
-func getLatestTag(sortedTags []string, currentTag string) string {
-	if len(sortedTags) == 0 {
-		return ""
+// getLatestTag returns the latest tag that is not a pre-release, or the current tag if no such tag exists
+func getLatestTag(tags []string, currentTag string) (string, error) {
+	currentVersion, err := version.NewVersion(currentTag)
+	if err != nil {
+		return "", err
 	}
-	latest := sortedTags[0]
-	parts := strings.Split(latest, ".")
-	dotCount := strings.Count(currentTag, ".")
-	return strings.Join(parts[:dotCount+1], ".")
+	versions := make([]*version.Version, len(tags))
+	for i, tag := range tags {
+		if versions[i], err = version.NewVersion(tag); err != nil {
+			return "", err
+		}
+	}
+
+	sort.Sort(sort.Reverse(version.Collection(versions)))
+
+	var latest *version.Version
+	for _, v := range versions {
+		if v.Prerelease() != "" {
+			continue
+		}
+
+		if v.GreaterThan(currentVersion) {
+			latest = v
+			break
+		}
+	}
+
+	if latest == nil {
+		return currentTag, nil
+	}
+
+	if isMajorOnly(currentTag) {
+		major := latest.Segments()[0]
+		if latest.Original()[0] == 'v' {
+			return fmt.Sprintf("v%d", major), nil
+		}
+		return fmt.Sprintf("%d", major), nil
+	}
+
+	return latest.Original(), nil
+}
+
+func isMajorOnly(v string) bool {
+	return !strings.Contains(v, ".")
 }
