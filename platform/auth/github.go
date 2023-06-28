fa5b78f721e54885f58e5288b3cf86ce314e2512
harryzcy <harry@harryzheng.com>
2023-06-27 22:53:47 -0500

    Use correct ghinstallation version

--- platform/auth/github.go
+++ platform/auth/github.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/harryzcy/snuuze/config"
 	"github.com/shurcooL/githubv4"
 	"golang.org/x/oauth2"
