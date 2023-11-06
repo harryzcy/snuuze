e1b1e17d34a359f7f406b2c7dc51e325723efb0d
Chongyi Zheng <git@zcy.dev>
2023-11-06 16:05:40 -0500

    Add docker manager to list

--- runner/manager/manager.go
+++ runner/manager/manager.go
@@ -2,6 +2,7 @@ package manager
 
 import (
 	"github.com/harryzcy/snuuze/runner/manager/common"
+	"github.com/harryzcy/snuuze/runner/manager/docker"
 	"github.com/harryzcy/snuuze/runner/manager/githubactions"
 	"github.com/harryzcy/snuuze/runner/manager/gomod"
 	"github.com/harryzcy/snuuze/runner/manager/pip"
@@ -10,6 +11,7 @@ import (
 
 // managers is a list of all supported package managers
 var managers = map[types.PackageManager]common.Manager{
+	types.PackageManagerDocker:        docker.New(),
 	types.PackageManagerGoMod:         gomod.New(),
 	types.PackageManagerGitHubActions: githubactions.New(),
 	types.PackageManagerPip:           pip.New(),
