7bcf024ccd06b9f0f492db1d0b5def6c912e487d
Chongyi Zheng <git@zcy.dev>
2023-11-06 03:00:24 -0500

    Add pip manager to list

--- runner/manager/manager.go
+++ runner/manager/manager.go
@@ -4,6 +4,7 @@ import (
 	"github.com/harryzcy/snuuze/runner/manager/common"
 	"github.com/harryzcy/snuuze/runner/manager/githubactions"
 	"github.com/harryzcy/snuuze/runner/manager/gomod"
+	"github.com/harryzcy/snuuze/runner/manager/pip"
 	"github.com/harryzcy/snuuze/types"
 )
 
@@ -11,4 +12,5 @@ import (
 var managers = map[types.PackageManager]common.Manager{
 	types.PackageManagerGoMod:         gomod.New(),
 	types.PackageManagerGitHubActions: githubactions.New(),
+	types.PackageManagerPip:           pip.New(),
 }
