f3fb4326b0cef5c39b3fb6b1768747ef116b7d78
Chongyi Zheng <git@zcy.dev>
2023-11-06 13:39:01 -0500

    Support parsing dockerfile

--- runner/manager/docker/docker.go
+++ runner/manager/docker/docker.go
@@ -2,9 +2,11 @@ package docker
 
 import (
 	"path/filepath"
+	"strings"
 
 	"github.com/harryzcy/snuuze/runner/manager/common"
 	"github.com/harryzcy/snuuze/types"
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
 )
 
 type DockerManager struct{}
@@ -23,7 +25,69 @@ func (m *DockerManager) Match(path string) bool {
 }
 
 func (m *DockerManager) Parse(match types.Match, data []byte) ([]*types.Dependency, error) {
-	return nil, nil
+	p := &parser.DirectiveParser{}
+	directives, err := p.ParseAll(data)
+	if err != nil {
+		return nil, err
+	}
+
+	dependencies := make([]*types.Dependency, 0)
+	for _, directive := range directives {
+		if directive.Name != "FROM" {
+			continue
+		}
+
+		image, version, versionType := parseDockerfileFromDirective(directive.Value)
+		dependencies = append(dependencies, &types.Dependency{
+			File:           match.File,
+			Name:           image,
+			Version:        version,
+			Indirect:       false,
+			PackageManager: types.PackageManagerDocker,
+			Position: types.Position{
+				Line:     directive.Location[0].Start.Line,
+				ColStart: directive.Location[0].Start.Character,
+				ColEnd:   directive.Location[0].End.Character,
+			},
+			Extra: map[string]interface{}{
+				"versionType": versionType,
+			},
+		})
+	}
+
+	return dependencies, nil
+}
+
+// parseDockerfileFromDirective parses a FROM directive in a Dockerfile.
+//
+// The value of the directive is expected to be in the format of:
+// [--platform=<platform>] <image>[:<tag>] [AS <name>] or
+// [--platform=<platform>] <image>[@<digest>] [AS <name>]
+//
+// Returns the image, version, and version type (tag or digest).
+func parseDockerfileFromDirective(value string) (image, version, versionType string) {
+	parts := strings.Split(value, " ")
+
+	for _, part := range parts {
+		part = strings.TrimSpace(part)
+		if part == "" || strings.HasPrefix(part, "--") {
+			continue
+		}
+
+		if strings.Contains(part, ":") {
+			image = strings.Split(part, ":")[0]
+			version = strings.Split(part, ":")[1]
+			versionType = "tag"
+		} else if strings.Contains(part, "@") {
+			image = strings.Split(part, "@")[0]
+			version = strings.Split(part, "@")[1]
+			versionType = "digest"
+		} else {
+			image = part
+		}
+	}
+
+	return image, version, versionType
 }
 
 func (m *DockerManager) FindDependencies(matches []types.Match) ([]*types.Dependency, error) {
