2f2e0c9a2457facc7fd44e365759e4093667330a
Chongyi Zheng <git@zcy.dev>
2024-02-11 04:14:30 -0500

    Fix build

--- server/job/cron.go
+++ server/job/cron.go
@@ -6,21 +6,20 @@ import (
 	"github.com/go-co-op/gocron/v2"
 )
 
-func StartCron(state *State) (*gocron.Scheduler, error) {
-	s := gocron.NewScheduler(time.UTC)
-	s.WaitForScheduleAll()
+func StartCron(state *State) (gocron.Scheduler, error) {
+	s, _ := gocron.NewScheduler()
 
-	_, err := s.Every(1).Hour().Do(func() {
+	_, err := s.NewJob(gocron.DurationJob(time.Hour), gocron.NewTask(func() {
 		checkUpdates(state)
-	})
+	}))
 	if err != nil {
 		return nil, err
 	}
 
-	s.StartAsync()
+	s.Start()
 	return s, nil
 }
 
-func StopCron(s *gocron.Scheduler) {
-	s.Stop()
+func StopCron(s gocron.Scheduler) {
+	s.Shutdown()
 }
